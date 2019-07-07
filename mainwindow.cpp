10993c87c25317a613994eeb3ed332da13c9ca27
BoOot <tcuZsgj@gmail.com>
2019-07-07 20:41:11 +0800

    finish open file button function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QString>
+#include <QFileDialog>
+#include <QTextCodec>
+
+FILE * fp = NULL;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,17 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actiontianjiawenjian_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName();
+    QTextCodec * codec = QTextCodec::codecForName("GBK");
+    char * file = codec->fromUnicode(fileName).data();
+    //file = NULL;
+    fp = fopen(file,"r");
+    if(!fp) {
+        ui->label_4->setText(codec->toUnicode("OPEN FILE ERROP!"));//label_4显示中文乱码
+        return;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actiontianjiawenjian_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
